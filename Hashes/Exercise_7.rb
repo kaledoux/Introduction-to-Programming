a7b9ad5d82d770de8f32417cbf93ea086645986d
Kyle Ledoux <kaledoux@gmail.com>
2019-03-10 08:56:17 -0400

    last exercise for ItP Hashes

---
+++ Hashes/Exercise_7.rb
@@ -0,0 +1,9 @@
+#Given the following error, what do you suspect is the most likely problem?
+
+#Error:
+#NoMethodError: undefined method 'keys' for Array
+
+#I suspect that the user most likely tried to call the .keys method on an Array
+#This method is used with hashes because values stored in hashes are paired with keys
+#In Arrays however, values are stored with indexes, so the keys method will not function
+
